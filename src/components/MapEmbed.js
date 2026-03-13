3c36d7232d8c3f352cf29d03a4960fc39aaee7ea
PawelStyczen <design@pawelstyczen.com>
2026-03-13 12:19:04 +0100

    fix to map embed

--- src/components/MapEmbed.js
+++ src/components/MapEmbed.js
@@ -8,7 +8,7 @@ const LOCATION_EMBEDS = {
   Tarnobrzeg:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2533.9841194657724!2d21.682501077017033!3d50.57165937862858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473d4a1d754b16c5%3A0xfa25173a98f1b1db!2sFITNESKA!5e0!3m2!1spl!2spl!4v1765495774599!5m2!1spl!2spl",
 
-  "SP Ulanów":
+  "GCK Ulanów":
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2538.23496891221!2d22.271714877013682!3d50.492584484328496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473cd45eba99b609%3A0x36344a83a5425ebd!2sPubliczna%20Szko%C5%82a%20Podstawowa%20W%20Ulanowie!5e0!3m2!1spl!2spl!4v1765496086157!5m2!1spl!2spl",
 };
 
